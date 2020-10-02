bdaf932d4847dc8459e59ed23cb31a404bbf72b3
Fauricio Valencia <julian.f.valencia@hotmail.com>
2020-10-01 23:12:33 -0500

    Implementancion de prueba de los componentes Button y Text

---
+++ src/container/App.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import {SafeAreaView, StyleSheet, ScrollView, StatusBar} from 'react-native'
+
+import {Colors} from 'react-native/Libraries/NewAppScreen'
+
+import {Button, Text} from '../components'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle='dark-content' />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior='automatic'
+          style={styles.scrollView}>
+          <Text label='hola' />
+          <Button label='Click' />
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+})
+
+export default App
